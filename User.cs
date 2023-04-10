78e559d78910d012c94feb47536dce39a8efc653
Naturalson001 <123705243+Naturalson001@users.noreply.github.com>
2023-04-10 14:54:39 +0100

    Add files via upload

---
+++ User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace User_Registration.Data
+{
+    [Index(nameof(User.Email), IsUnique = true)]
+    public class User
+	{
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Firstname is required.")]
+        public string Firstname { get; set; }
+
+        [Required(ErrorMessage = "Lastname is required.")]
+        public string Lastname { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Phone Number is required.")]
+        [Phone(ErrorMessage = "Invalid phone number format.")]
+        public string PhoneNumber { get; set; }
+
+
+
+    }
+
+
+}
+
+
