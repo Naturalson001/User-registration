a71384306a25305c109b48b8c13ef83bc870f750
Naturalson001 <123705243+Naturalson001@users.noreply.github.com>
2023-04-10 14:54:21 +0100

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+
+namespace User_Registration.Data
+{
+	public class ApplicationDbContext:DbContext
+	{
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationDbContext(string connectionString) 
+        {
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL("server=localhost;Database=Mydatabase username= root;port=3306;password=Ayevbosaimade1");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // ensure email addresses are unique
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+        }
+    }
+}
+
