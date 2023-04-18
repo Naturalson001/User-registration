54145108015a637640b7c888fd886b9daeb28e5a
Naturalson001 <123705243+Naturalson001@users.noreply.github.com>
2023-04-18 01:57:23 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ using System.Security.Cryptography;
 using System.Text;
 namespace User_Registration;
 using System.Net.Mail;
+using System.Runtime.Intrinsics.Arm;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using MySql.Data.MySqlClient;
 using Mysqlx.Crud;
 using User_Registration.Data;
@@ -19,13 +21,12 @@ class Program
 
     static void Main(string[] arg)
     {
-
-      
+     
         try
         {
             var userData = new Dictionary<string, string>();
 
-            string userId = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+            string userId = Guid.NewGuid().ToString();
             Console.Write("Enter your Firstname: ");
             string firstname = Console.ReadLine();
             userData.Add("Firstname", firstname);
@@ -40,20 +41,22 @@ class Program
 
             Console.Write("Enter your Password: ");
             string password = Console.ReadLine();
-            userData.Add("Password", password);
             string hashedPassword = HashPassword(password);
+
+            userData.Add("Password", hashedPassword);
+
 
             Console.Write("Enter your Phone Number: ");
             string phonenumber = Console.ReadLine();
             userData.Add("PhoneNumber", phonenumber);
-            string tableName = "users";
-            string[] columns = { "Userid", "Firstname", "lastname", "emai", " Password", "PhoneNumber" };
-            string[] values = {   userId,  firstname, lastname, mail, password, phonenumber };
+            string tableName = "USER";
+            string[] columns = { "Userid", "Firstname", "lastname", "email", " hashedPassword", "PhoneNumber" };
+            string[] values = {   userId,  firstname, lastname, mail, hashedPassword, phonenumber };
             string columnList = string.Join(", ", columns);
             string valueList = string.Join(", ", values.Select(v => $"'{v}'"));
             string query = string.Format("INSERT INTO {0} ({1}) VALUES ({2})", tableName, columnList, valueList);
 
-            string connectionString = @"server=localhost;uid=root;pwd=Ayevbosaimade1;database=project;";
+            string connectionString = @"server=localhost;uid=root;pwd=Ayevbosaimade1;database=Project;";
             MySqlConnection connection = new MySqlConnection(connectionString);
 
             MySqlCommand command = new MySqlCommand(query, connection);
@@ -90,28 +93,44 @@ class Program
 
         Console.Read();
     }
-    static string HashPassword(string password)
+
+   
+  
+
+        public void ConfigureServices(IServiceCollection services)
+            => services.AddDbContext<ApplicationDbContext>();
+
+  
+
+    static string  HashPassword(string password)
     {
-        using (var sha256 = SHA256.Create())
-        {
-            var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
-            return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
-        }
+        byte[] salt = new byte[16];
+        new RNGCryptoServiceProvider().GetBytes(salt);
+
+        var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 10000, HashAlgorithmName.SHA256);
+        byte[] hash = pbkdf2.GetBytes(20);
+
+        byte[] hashBytes = new byte[36];
+        Array.Copy(salt, 0, hashBytes, 0, 16);
+        Array.Copy(hash, 0, hashBytes, 16, 20);
+
+        return Convert.ToBase64String(hashBytes);
+
+
     }
+
     static void RegisterUser(Dictionary<string, string> userData, string connectionString)
     {
-        using (var context = new ApplicationDbContext(connectionString))
+        using var context = new ApplicationDbContext(connectionString);
+        var user = new User
         {
-            var user = new User
-            {
-                Firstname = userData["Firstname"],
-                Lastname = userData["Lastname"],
-                Email = userData["Email"],
-                Password = userData["Password"],
-                PhoneNumber = userData["PhoneNumber"]
-            };
-           
-        }
+            Firstname = userData["Firstname"],
+            Lastname = userData["Lastname"],
+            Email = userData["Email"],
+            hashedPassword = userData["Password"],
+            PhoneNumber = userData["PhoneNumber"]
+
+        };
 
     }
    
